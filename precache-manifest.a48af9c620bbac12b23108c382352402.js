7f994e42c196ad3dd6836a34d09de516dae34c48
Naseem <naseemshahvarikodan@gmail.com>
2020-04-21 14:24:57 +0530

    Updates

---
+++ precache-manifest.a48af9c620bbac12b23108c382352402.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b6c46ac93ea9273de8808b2a6a430a",
+    "url": "/WeatherX/index.html"
+  },
+  {
+    "revision": "b885afa00e6aa0666251",
+    "url": "/WeatherX/static/css/main.5b9e35e1.chunk.css"
+  },
+  {
+    "revision": "01f6464ca3dc397f916c",
+    "url": "/WeatherX/static/js/2.8a877a26.chunk.js"
+  },
+  {
+    "revision": "b885afa00e6aa0666251",
+    "url": "/WeatherX/static/js/main.8cadd21f.chunk.js"
+  },
+  {
+    "revision": "85906204cfc079e80b75",
+    "url": "/WeatherX/static/js/runtime~main.1029ae92.js"
+  },
+  {
+    "revision": "564a2354a18c400317803fba7bac714e",
+    "url": "/WeatherX/static/media/humid.564a2354.svg"
+  },
+  {
+    "revision": "de59412e52ebb1e2a81266bf6d96eba2",
+    "url": "/WeatherX/static/media/location.de59412e.svg"
+  },
+  {
+    "revision": "c10d1645d478bd314cd34c1377f6436c",
+    "url": "/WeatherX/static/media/search-icon.c10d1645.svg"
+  }
+]);
