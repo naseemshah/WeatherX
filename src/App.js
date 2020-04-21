68d78361e1db891d5dd6b3ccf86573c14bc7a3dd
Naseem <naseemshahvarikodan@gmail.com>
2020-04-21 14:33:01 +0530

    changed http:// to https://

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends React.Component {
 
       
       if (city && country){
-            const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=${units}`);
+            const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=${units}`);
             const data = await api_call.json();
             console.log(data);
             if(!(data.message)){
@@ -87,7 +87,7 @@ class App extends React.Component {
             }
     }
       else if(city && !country) {
-            const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`);
+            const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`);
             const data = await api_call.json();
             console.log(data);
             if(!(data.message)){
